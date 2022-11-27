1b93a79256f5c5b116dbf06a19be978d0fb05f0c
yuliakudryavtseva <Julia.Miasoiedova@iocea.com>
2022-11-27 22:48:10 +0000

    basic styling applied

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <CurrencyConverter />
+  </div>
 </template>
 
 <script>
+import CurrencyConverter from "./components/CurrencyConverter";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    CurrencyConverter,
+  },
 };
 </script>
 
 <style>
+@font-face {
+  font-family: "Short Stack Regular";
+  src: url("./assets/fonts/ShortStack-Regular.ttf");
+  font-display: swap;
+}
+body {
+  font-family: "Short Stack Regular", "Franklin Gothic Medium", "Arial Narrow",
+    Arial, sans-serif;
+  width: 800px;
+  margin: auto;
+  letter-spacing: 1px;
+}
+@media (max-width: 810px) {
+  body {
+    width: 500px;
+  }
+}
+@media (max-width: 510px) {
+  body {
+    width: 300px;
+  }
+}
 </style>
---
+++ src/components/CurrencyConverter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Currency Converter</h1>
+    <form>
+      <label for="from-currency">From</label>
+      <select name="" id="from-currency">
+        <option value="USD">USD</option>
+        <option value="UAH">UAH</option>
+      </select>
+      <label for="to-currency">To</label>
+      <select name="" id="to-currency">
+        <option value="USD">USD</option>
+        <option value="UAH">UAH</option>
+      </select>
+      <input type="number" min="0" name="" id="" placeholder="Amount..." />
+      <input type="submit" value="Convert" />
+    </form>
+    <div class="result">
+      <p>Result: your result</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CurrencyConverter",
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: darkcyan;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+label {
+  font-size: 20px;
+}
+select,
+input {
+  height: 50px;
+  margin: 20px 0;
+  padding: 0 20px;
+  background: #d3d3d33d;
+  border: none;
+  color: #000;
+  font-size: 18px;
+  border-radius: 10px;
+}
+input[type="submit"] {
+  cursor: pointer;
+  background: lightseagreen;
+  color: #fff;
+  font-size: 20px;
+  text-transform: uppercase;
+}
+</style>
